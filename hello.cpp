d7e2f179951c03101bcf9fc9458e9a892138b1b4
tapia-ops <248371069+tapia-ops@users.noreply.github.com>
2026-05-10 22:25:25 +0800

    Initial Commit

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// This is a single-line comment
+// It explain what the program does
+
+/*
+This is a multi-line comment
+You can write multiple lines of explanation
+Without worrying about adding // to each line
+*/
+int main() {
+    // Print a greeting message
+    std::cout << "Hello, World!" << std::endl;
+     /*
+    You can also use multi-line comments in 
+    the middle of the code if needed
+    */
+    return 0; //Return success status to the operating system
+   
+}
---
+++ hello2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main () {
+    // Using std::endl for line breaks
+    std::cout << "Hello, World" << std::endl;
+    std::cout << "Welcome to the C++ Programming!" << std::endl;
+
+    // Using \n for line breaks
+    std::cout << "Using newline character:\n";
+    std::cout << "Line 1\n";
+    std::cout << "Line 2\n";
+
+    // Mixing both methods
+    std::cout << "Mixing both methods" << std::endl;
+    std::cout << "Line with endl" << std::endl;
+    std::cout << "Line with newline character\n";
+
+    return 0; 
+}
+
